8a5ba68304e81ce7943c20001602fc35245490ae
XueJieyu <snow@snow.(none)>
2015-11-06 18:10:21 +0800

    day5lab

--- atomic_ops.c
+++ atomic_ops.c
@@ -44,8 +44,11 @@ inode_nr search_dentry(inode_nr ino, char *name)
 {
 	unsigned int blkcnt,offset,info[3];
 	__u64 inoaddr;
+	__u64 blkaddr;
+	block_nr *p;
 	struct neo_inode dirinode;
 	struct neo_dir_entry dentry;
+	int i,n;
 	inoaddr = inode_to_addr(ino);
 	fseek(fp,inoaddr,SEEK_SET);
 	fread(&dirinode,neo_sb_info.s_inode_size,1,fp);
@@ -53,8 +56,69 @@ inode_nr search_dentry(inode_nr ino, char *name)
 	if (dirinode.blocks == 0)
 		return 0;
 	blkcnt = dirinode.blocks;
-	return 0;
+	if (blkcnt <= 12)
+		n = blkcnt;
+	else
+		n = 12;
+	for (i = 0; i < n; i++){
+		blkaddr = block_to_addr(dirinode.block[i]);
+		if (blk_search_dentry(blkaddr,name,info) == 0)
+			return 0;
+	}
+	if (blkcnt > 12){//dir file's max blocks count is 13,block[12] for indirect addr.
+		n = blkcnt - 12;
+		p = (__u32 *)malloc(4 * n);	//4 = sizeof(__32)
+		fseek(fp,block_to_addr(dirinode.block[12]),SEEK_SET);
+		fread(p,(4 * n),1,fp);
+		for (i = 0, i < n, i++){
+			blkaddr = block_to_addr(p[i]);
+			if (blk_search_dentry(blkaddr,name,info) == 0)
+				return 0;
+		}
+	}
+	free(p);
 }
+
+int blk_search_dentry(__64 blkaddr,char *name,unsigned int info[])
+{
+
+	unsigned int offset_prev = 0;		//记录块内上一个目录项的偏移；
+	unsigned int offset_cur = 0;		//记录块内当前目录项的偏移；
+	unsigned int order = 0;			//当前目录项是这个块内的第几个。以上对应info3个成员，需要维护这3个值
+	unsigned short true_len;		//计算record的真实长度
+	unsigned char length;			//计算name的真实长度
+	struct neo_dir_entry *cur;		//临时存放读取的条目
+	void *block;
+
+	block = (void *)malloc(BLOCK_SIZE);
+	fseek(fp,blkaddr,SEEK_SET);
+	fread(block,BLOCK_SIZE,1,fp);		//将此块读入内存
+
+	cur = block;
+	if(cur->name_len == 0)			//第一个是空块
+		(char *)block += cur->rec_len;
+	offset_prev += cur->rec_len;
+	offset_cur += cur->rec_len;
+	cur = block;
+	block += 8;
+	order++;
+	while ((offset_cur + cur->rec_len) != 4096 ){		//当cur下一项还有
+		length = (4 - cur->name_len%4) + cur->name_len;	//计算此name实际长度
+		true_len = 8 + length;
+		cur = block;
+		if(cur->name)
+			info <- prev,cur,tmp->inode,order;
+			return 0;
+		offset_prev = offset_cur;
+		offset_cur += tmp->rec_len;
+		fseek(fp, tmp->rec_len – true_len, SEEK_CUR);//指向下一个记录
+		fread(tmp,8,1,fp);//至此cur已指向第一个目录项
+		order++;//0 + 1 = 1
+	}//循环跳出后再把最后一个目录项比较一下。完成~
+
+
+}
+
 
 block_nr get_block(char *path)
 {
@@ -127,6 +191,11 @@ __u64 inode_to_addr(inode_nr ino)
 	offset += (BLOCK_SIZE * BLOCKS_PER_GROUP * groupnr + r * neo_sb_info.s_inode_size);
 	//printf("inode %d addr is %d\n\n",ino,offset);
 	return offset;
+}
+
+__u64 inline block_to_addr(block_nr blk)
+{
+	return (blk * 4096);
 }
 
 int is_powerof_357(int i)
